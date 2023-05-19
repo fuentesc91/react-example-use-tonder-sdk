713fcc83017bd5a88e3cda5b6042bad8de3d207f
Carlos Fuentes <fuentesc91@gmail.com>
2023-05-19 10:17:33 -0600

    Updates

--- inlinesdk.js
+++ inlinesdk.js
@@ -78,180 +78,176 @@ class InlineCheckout {
     const injectInterval = setInterval(() => {
       if (document.querySelector("#tonder-checkout")) {
         document.querySelector("#tonder-checkout").innerHTML = `
-        <div class="container-tonder">
-            <p class="p-card-tonder">Titular de la tarjeta</p>
-            <div id="collectCardholderNameTonder" class="empty-div-tonder"></div>
-            <p class="p-card-tonder"> Información de la tarjeta</p>
-            <div id="collectCardNumberTonder" class="empty-div-tonder"></div>
-            <div class="collect-row-tonder">
-                <div id="collectExpirationMonthTonder" class="empty-div-dates-tonder"></div>
-                <div id="collectExpirationYearTonder" class="empty-div-dates-tonder"></div>
-                <div id="collectCvvTonder" class="empty-div-cvc-tonder"></div>
-            </div>
-            <div id="msgError"></div>
-            <div>
-            <p class="politics-p-tonder">
-                Tus datos personales se utilizarán para procesar tu pedido, respaldar tu
-                experiencia a través de este sitio web y otros fines descritos en nuestra
-                <a  class="link-terms-tonder" href="<?php echo esc_url($url_politicas_tonder); ?>" target="_blank">política de privacidad</a>.
-            </p>
-            <br>
-            <div class="container-politics-tonder">
-                <input type="checkbox" id="acceptTonder" name="scales" checked>
-                <label class="terms-label-tonder" for="scales">
-                    He leído y estoy de acuerdo con los
-                    <a class="link-terms-tonder" href="<?php echo esc_url($url_terminos_tonder); ?>" target="_blank">términos y condiciones</a>
-                    de este sitio web.
-                </label>
-            </div>
-        </div>
-            <button id="tonderPayButton" class="payButton">Pagar</button>
-        </div>
+  <div class="container-tonder">
+      <p class="p-card-tonder">Titular de la tarjeta</p>
+      <div id="collectCardholderNameTonder" class="empty-div-tonder"></div>
+      <p class="p-card-tonder"> Información de la tarjeta</p>
+      <div id="collectCardNumberTonder" class="empty-div-tonder"></div>
+      <div class="collect-row-tonder">
+          <div id="collectExpirationMonthTonder" class="empty-div-dates-tonder"></div>
+          <div id="collectExpirationYearTonder" class="empty-div-dates-tonder"></div>
+          <div id="collectCvvTonder" class="empty-div-cvc-tonder"></div>
+      </div>
+      <div id="msgError"></div>
+      <div>
+      <div class="container-politics-tonder">
+          <input type="checkbox" id="acceptTonder" name="scales" checked>
+          <label class="terms-label-tonder" for="scales">
+              He leído y estoy de acuerdo con los
+              <a class="link-terms-tonder" href="<?php echo esc_url($url_terminos_tonder); ?>" target="_blank">términos y condiciones</a>
+              de este sitio web.
+          </label>
+      </div>
+  </div>
+      <button id="tonderPayButton" class="payButton">Pagar</button>
+  </div>
 
-        <style>
-            .container-tonder {
-                width: 90% !important;
-                font-family: "Arial", sans-serif !important;
-                margin: 0 auto !important;
-                max-height: 0px;
-                overflow: hidden;
-                transition: max-height 0.5s ease-out;
-                max-width: 600px
-            }
+  <style>
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap');
+      .container-tonder {
+          width: 90% !important;
+          font-family: 'Inter', sans-serif;
+          margin: 0 auto !important;
+          max-height: 0px;
+          overflow: hidden;
+          transition: max-height 0.5s ease-out;
+          max-width: 600px;
+      }
 
-            .container-selected {
-                max-height: 100vh;
-            }
-    
-            .p-card-tonder {
-                font-weight: bold !important;
-                font-size: 13px !important;
-                margin: 0 !important;
-                padding: 0 !important;
-            }
-    
-            .payment_method_zplit {
-                font-size: 16px !important;
-                width: 100% !important;
-            }
-    
-            .payment_method_zplit  label img {
-                width: 68px !important;
-                padding-left: 1px !important;
-            }
-    
-            .container-politics-tonder {
-                display: flex !important;
-                align-items: center !important;
-                margin-bottom: 2rem;
-            }
-    
-            .politics-p-tonder {
-                font-size: 13px !important;
-                margin: 0 !important;
-            }
-    
-            .terms-label-tonder {
-                font-size: 12px !important;
-                margin: 0 0 0 10px !important;
-            }
-    
-            .collect-row-tonder {
-                display: flex !important;
-                justify-content: space-between !important;
-                width: 100% !important;
-            }
-    
-            .collect-row-tonder > div {
-                width: calc(25% - 10px) !important;
-            }
-    
-            .collect-row-tonder > div:last-child {
-                width: 50% !important;
-            }
-    
-            .empty-div-tonder {
-                height: 65px !important;
-            }
-    
-            .empty-div-dates-tonder {
-                height: 90px !important;
-            }
-    
-            .empty-div-cvc-tonder {
-                height: 90px !important;
-            }
-    
-            .reveal-view {
-                margin-top: 0px !important;
-            }
-    
-            .error-tonder-container-tonder{
-                color: red !important;
-                background-color: #FFDBDB !important;
-                margin-bottom: 13px !important;
-                font-size: 80% !important;
-                padding: 8px 10px !important;
-                border-radius: 10px !important;
-                text-align: left !important;
-            }
-    
-            .image-error-tonder {
-                width: 14px !important;
-                margin: -2px 5px !important;
-            }
-    
-            .link-terms-tonder {
-                color: black !important;
-            }
-    
-            .link-terms-tonder:hover {
-                text-decoration: None !important;
-                color: black !important;
-            }
-            .payButton {
-                min-height: 2.3rem;
-                border-radius: 0.5rem;
-                cursor: pointer;
-                width: 100%;
-                padding: 1rem;
-                text-align: center;
-                border: none;
-                background-color: #000;
-                color: #fff;
-            }
-    
-            @media screen and (max-width: 600px) {
-                .p-card-tonder {
-                    font-weight: bold !important;
-                    font-size: 13px !important;
-                    margin: 0 !important;
-                    padding: 0 !important;
-                }
-    
-                .payment_method_zplit {
-                    font-size: 16px !important;
-                    width: 100% !important;
-                }
-    
-                .payment_method_zplit  label img {
-                    display: none !important;
-                }
-    
-                .empty-div-dates-tonder {
-                    height: 90px !important;
-                    width: 60px !important;
-                }
-    
-                .empty-div-cvc-tonder {
-                    height: 90px !important;
-                    width: 130px !important;
-                }
-    
-            }
-    
-        </style>
-    `;
+      .container-selected {
+          max-height: 100vh;
+      }
+
+      .p-card-tonder {
+          font-weight: bold !important;
+          font-size: 13px !important;
+          text-align: left;
+      }
+
+      .payment_method_zplit {
+          font-size: 16px !important;
+          width: 100% !important;
+      }
+
+      .payment_method_zplit  label img {
+          width: 68px !important;
+          padding-left: 1px !important;
+      }
+
+      .container-politics-tonder {
+          display: flex !important;
+          align-items: center !important;
+          margin-bottom: 2rem;
+      }
+
+      .politics-p-tonder {
+          font-size: 13px !important;
+          margin: 0 !important;
+      }
+
+      .terms-label-tonder {
+          font-size: 14px !important;
+          margin: 0 0 0 10px !important;
+      }
+
+      .collect-row-tonder {
+          display: flex !important;
+          justify-content: space-between !important;
+          width: 100% !important;
+      }
+
+      .collect-row-tonder > div {
+          width: calc(25% - 10px) !important;
+      }
+
+      .collect-row-tonder > div:last-child {
+          width: 50% !important;
+      }
+
+      .empty-div-tonder {
+          height: 65px !important;
+      }
+
+      .empty-div-dates-tonder {
+          height: 90px !important;
+      }
+
+      .empty-div-cvc-tonder {
+          height: 90px !important;
+      }
+
+      .reveal-view {
+          margin-top: 0px !important;
+      }
+
+      .error-tonder-container-tonder{
+          color: red !important;
+          background-color: #FFDBDB !important;
+          margin-bottom: 13px !important;
+          font-size: 80% !important;
+          padding: 8px 10px !important;
+          border-radius: 10px !important;
+          text-align: left !important;
+      }
+
+      .image-error-tonder {
+          width: 14px !important;
+          margin: -2px 5px !important;
+      }
+
+      .link-terms-tonder {
+          color: black !important;
+      }
+
+      .link-terms-tonder:hover {
+          text-decoration: None !important;
+          color: black !important;
+      }
+      .payButton {
+          font-size: 16px;
+          font-weight: bold;
+          min-height: 2.3rem;
+          border-radius: 0.5rem;
+          cursor: pointer;
+          width: 100%;
+          padding: 1rem;
+          text-align: center;
+          border: none;
+          background-color: #000;
+          color: #fff;
+      }
+
+      @media screen and (max-width: 600px) {
+          .p-card-tonder {
+              font-weight: bold !important;
+              font-size: 13px !important;
+              margin: 0 !important;
+              padding: 0 !important;
+          }
+
+          .payment_method_zplit {
+              font-size: 16px !important;
+              width: 100% !important;
+          }
+
+          .payment_method_zplit  label img {
+              display: none !important;
+          }
+
+          .empty-div-dates-tonder {
+              height: 90px !important;
+              width: 60px !important;
+          }
+
+          .empty-div-cvc-tonder {
+              height: 90px !important;
+              width: 130px !important;
+          }
+
+      }
+
+  </style>
+`;
         this.fetchTonderData();
         clearInterval(injectInterval);
       }
@@ -295,7 +291,8 @@ class InlineCheckout {
       businessPkTonder = dataBusinessTonder.business.pk;
 
       // Openpay
-      openpayMerchantIdTonder = dataBusinessTonder.openpay_keys.merchant_id;
+      openpayMerchantIdTonder =
+        dataBusinessTonder.openpay_keys.merchant_id;
       openpayPublicKeyTonder = dataBusinessTonder.openpay_keys.public_key;
     } catch (error) {
       console.log(error);
@@ -407,14 +404,15 @@ class InlineCheckout {
       type: Skyflow.ElementType.EXPIRATION_YEAR,
     });
 
-    const cardHolderNameElementTonder = await collectContainerTonder.create({
-      table: "cards",
-      column: "cardholder_name",
-      ...collectStylesOptionsTonder,
-      label: "",
-      placeholder: "Nombre como aparece en la tarjeta",
-      type: Skyflow.ElementType.CARDHOLDER_NAME,
-    });
+    const cardHolderNameElementTonder =
+      await collectContainerTonder.create({
+        table: "cards",
+        column: "cardholder_name",
+        ...collectStylesOptionsTonder,
+        label: "",
+        placeholder: "Nombre como aparece en la tarjeta",
+        type: Skyflow.ElementType.CARDHOLDER_NAME,
+      });
 
     // Mount the elements.
     cardNumberElementTonder.mount("#collectCardNumberTonder");
@@ -506,7 +504,8 @@ class InlineCheckout {
       if (!checkboxTonder.checked) {
         var msgErrorDiv = document.getElementById("msgError");
         msgErrorDiv.classList.add("error-tonder-container-tonder");
-        msgErrorDiv.innerHTML = "Necesitas aceptar los términos y condiciones";
+        msgErrorDiv.innerHTML =
+          "Necesitas aceptar los términos y condiciones";
         setTimeout(function () {
           document.querySelector("#tonderPayButton").disabled = false;
           msgErrorDiv.classList.remove("error-tonder-container-tonder");
@@ -554,7 +553,9 @@ class InlineCheckout {
           date: dateString,
           order: jsonResponseOrder.id,
         };
-        const jsonResponsePayment = await createPaymentTonder(paymentItems);
+        const jsonResponsePayment = await createPaymentTonder(
+          paymentItems
+        );
 
         // Checkout router
         const routerItems = {
